95a80cb1e2604e72da3dff9aa4e7d063ad26402d
Mihai Potra <mike@mpotra.com>
2021-09-20 11:44:20 +0300

    Remove editor configs

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,4 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,examples,lib,test}/**/*.{ex,exs}"],
-  line_length: 98
+  inputs: ["{mix,.formatter}.exs", "{config,examples,lib,test}/**/*.{ex,exs}"]
 ]
